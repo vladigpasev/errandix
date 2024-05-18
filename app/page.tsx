e86bfb975d0736762c99d601c72fe5aff24c4283
Vladimir Pasev <vladigpasev@gmail.com>
2024-05-18 09:46:51 +0300

    Fixed search bar

--- app/page.tsx
+++ app/page.tsx
@@ -18,17 +18,13 @@ async function Home() {
               <span className="text-white sm:text-5xl text-4xl font-semibold ">, която ти трябва лесно</span>
             </h1>
             <div className='pt-8'>
-              <form className="max-w-lg">
-                <div className="flex">
-                  <div className="relative z-1 page-content w-full">
-                    <input type="search" id="search-dropdown" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border-s-gray-50 border-s-2 border border-gray-300 " placeholder="Търси всякакви услуги..." required />
-                    <Link href='/findjob' type="submit" className="sm:absolute top-0 end-0 p-2.5 text-sm font-medium sm:h-full sm:m-0 mt-2 text-white bg-secondary sm:rounded-e-lg border border-secondary hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-secondary sm:w-auto w-full h-10 rounded">
-                      <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                      </svg>
-                      <span className="sr-only">Търсене</span>
-                    </Link>
-                  </div>
+              <form className="max-w-xl">
+                <div className="flex w-full">
+                  <Link href='/findjob' className='w-full'>
+                    <div className="z-1 page-content w-full">
+                      <input type="search" id="search-dropdown" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border-s-gray-50 border-s-2 border border-gray-300 " placeholder="Търси всякакви услуги..." required />
+                    </div>
+                  </Link>
                 </div>
               </form>
               <div className='sm:flex hidden items-start gap-5 pt-8 text-white text-sm font-medium'>
